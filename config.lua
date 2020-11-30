f5d86630368924f1307aeec02f8ee0f66c405201
MasterLua <60834623+MasterLua@users.noreply.github.com>
2020-11-30 16:12:50 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -7,7 +7,7 @@ Config.RateLimit = 35 -- Per 10s
 Config.ResetRateLimit = 5000 -- 10000 = 10s it's in milliseconds
 
 --LOGS
-Config.WebhookLink = "https://discord.com/api/webhooks/782336358391611422/72yKnW9UAwP57f8CX-NHZcP4FCzB-VjWHwyOab7z3P2RSx2BTxIbvwfKwLe3OUFoKaNF"
+Config.WebhookLink = "YourEmail"
 
 --KICK
 Config.KickMessage = "Vous n'êtes pas autorisé d'exécuter autant de requête en peu de temps. ( Cela est peut etre une erreur )"
