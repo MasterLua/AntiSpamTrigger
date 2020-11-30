77d760d6de54d82c81a82adcfb215aed1269f219
MasterLua <60834623+MasterLua@users.noreply.github.com>
2020-11-30 16:13:09 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -7,8 +7,7 @@ Config.RateLimit = 35 -- Per 10s
 Config.ResetRateLimit = 5000 -- 10000 = 10s it's in milliseconds
 
 --LOGS
-Config.WebhookLink = "YourEmail"
-
+Config.WebhookLink = "YourWebhook"
 --KICK
 Config.KickMessage = "Vous n'êtes pas autorisé d'exécuter autant de requête en peu de temps. ( Cela est peut etre une erreur )"
 
